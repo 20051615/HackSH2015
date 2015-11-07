3a4c89ab93d1f9475732c5d847752de6d904874b
zhuyifei1999 <zhuyifei1999@gmail.com>
2015-11-07 19:24:20 +0800

    Item: added constructor params & fromJSON

--- HackSH2015/src/tk/shsidcomputers/hacksh2015/Item.java
+++ HackSH2015/src/tk/shsidcomputers/hacksh2015/Item.java
@@ -8,15 +8,19 @@ import org.json.JSONObject;
  * @author SHSIDComputerClub
  * Note: this class has a natural ordering that is inconsistent with equals.
  */
-public class Item implements Comparable<Item>{
+public class Item implements Comparable<Item> {
 	private String title, details;
 	private Date mustStartDate, dueDate;
 	private boolean isDone;
 	private Priority priority;
 	
-	public Item(String title, Date date) {
+	public Item(String title, String details, Date mustStartDate, Date dueDate, boolean isDone, Priority priority) {
 		this.title = title;
-		dueDate = date;
+		this.details = details;
+		this.mustStartDate = mustStartDate;
+		this.dueDate = dueDate;
+		this.isDone = isDone;
+		this.priority = priority;
 	}
 	
 	public int compareTo(Item otherItem) {
@@ -83,10 +87,21 @@ public class Item implements Comparable<Item>{
 		JSONObject obj = new JSONObject();
 		obj.put("title", title);
 		obj.put("details", details);
-		obj.put("mustStartDate", mustStartDate.getTime());
+		obj.put("mustStartDate", hasStartDate() ? mustStartDate.getTime() : null);
 		obj.put("dueDate", dueDate.getTime());
 		obj.put("isDone", isDone);
-		obj.put("priority", priority);
+		obj.put("priority", priority.ordinal());
 		return obj.toString();
+	}
+	
+	public static Item fromJSON(String json) {
+		JSONObject obj = new JSONObject(json);
+		String title = (String) obj.get("title");
+		String details = (String) obj.get("details");
+		Date mustStartDate = obj.get("mustStartDate") == null ? null : new Date((long) obj.get("mustStartDate"));
+		Date dueDate = new Date((long) obj.get("dueDate"));
+		boolean isDone = (boolean) obj.get("isDone");
+		Priority priority = Priority.values()[(int) obj.get("priority")];
+		return new Item(title, details, mustStartDate, dueDate, isDone, priority);
 	}
 }
