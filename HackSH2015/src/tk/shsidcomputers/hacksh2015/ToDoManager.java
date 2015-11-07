c82e581a3762206458d4dc133bae733e4175602e
zhuyifei1999 <zhuyifei1999@gmail.com>
2015-11-07 23:22:20 +0800

    ToDoManager: Organized imports

--- HackSH2015/src/tk/shsidcomputers/hacksh2015/ToDoManager.java
+++ HackSH2015/src/tk/shsidcomputers/hacksh2015/ToDoManager.java
@@ -1,23 +1,20 @@
 package tk.shsidcomputers.hacksh2015;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import java.awt.GridBagLayout;
-import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.Font;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JScrollPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 
 public class ToDoManager {
 
